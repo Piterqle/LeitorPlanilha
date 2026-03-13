230e0729466d6668f509e7314c69a6de84f4c5d9
Piterqle <pedrohenriquemendesmartins5@gmail.com>
2026-03-13 12:45:34 -0300

    Melhoria no GUI e organizando os arquivos

--- Experimentais/Back.py
+++ Experimentais/Back.py
@@ -6,6 +6,13 @@ from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter, range_boundaries
 from datetime import datetime
 
+def insert_Str(liststr , insert_format):
+    insert_format.sort(key=lambda x: x[0], reverse=True)
+    
+    for index, text in insert_format:
+        liststr.insert(index, text)
+    
+    return "".join(liststr)
 
 def openPath(self):
     if os.path.exists('Experimentais/caminho.json'):
@@ -44,10 +51,20 @@ def addAluno(aluno, modalidade, data_marcada, data_experiencia, horario, contato
             print("Preencha todos os campos.")
             return
         
-        if(datetime.strptime(data_marcada, "%d/%m/%Y") < datetime.now()):
+        if(datetime.strptime(data_experiencia, "%d/%m/%Y") < datetime.now()):
             print("A data marcada não pode ser anterior à data atual.")
             return
+        
+        if(contato[0] != "("):
+            liststr = list(contato)
+            insert_format = [(0, "("), (2, ")")]
+            contato = insert_Str(liststr, insert_format)
 
+        if(contato[11] != "-"):
+            list(contato)
+            insert_format = [(10, "-")]
+            contato = insert_Str(liststr, insert_format)
+        
         with open('Experimentais/caminho.json', 'r') as f:
             caminho = f.read()
             if caminho != "":
